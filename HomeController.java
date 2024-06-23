074cd5810f49663de9b450ae0e2fb88fc40525f3
HamedMiri72 <160745852+HamedMiri72@users.noreply.github.com>
2024-06-24 02:58:37 +0330

    Add files via upload
    with this code we can handel a request an write in jsp

--- HomeController.java
+++ HomeController.java
@@ -1,6 +1,9 @@
 package com.hamed.MyWebApp;
 
 
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -14,11 +17,17 @@ public class HomeController
     @RequestMapping("home")
     //@ResponseBody
     //ResposeBody says whatever you are returning is actually the data, its not the page and image data
-    public String home(){
-        System.out.println("hi world");
+    public String home(HttpServletRequest request, HttpServletResponse response){
+
+        HttpSession session = request.getSession();
+        //accept data from url, we should use HttpServletRequest as an object
+        String name = request.getParameter("name");
+        session.setAttribute("name", name );
+        System.out.println("hi world" + name);
+
         //controller you are responsible to call this home.jsp page
         return "home";
         //path of the page is defined by prefix and the extention of the page is defined with the help of surffix so we can remove .jsp
-        
+
     }
 }
