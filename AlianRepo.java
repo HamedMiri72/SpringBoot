e01260efcc73bffe7a775b6add5f9b91d0b3e952
HamedMiri72 <160745852+HamedMiri72@users.noreply.github.com>
2024-06-25 00:42:37 +0330

    Add files via upload

---
+++ AlianRepo.java
@@ -0,0 +1,11 @@
+package com.hamed.bootjpa.dao;
+
+import com.hamed.bootjpa.model.Alien;
+import org.springframework.data.repository.CrudRepository;
+
+public interface AlianRepo extends CrudRepository<Alien, Integer>
+{
+
+
+
+}
