4a8ab0245c6b1a162aa9f80f0f60a10840d6367d
HamedMiri72 <160745852+HamedMiri72@users.noreply.github.com>
2024-06-27 00:08:49 +0330

    Add files via upload

---
+++ QuestionDao.java
@@ -0,0 +1,11 @@
+package com.hamed.quizzappfinal.dao;
+
+import com.hamed.quizzappfinal.model.Question;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface QuestionDao extends JpaRepository<Question, Integer>
+{
+    List<Question> findByCategory(String category);
+}
